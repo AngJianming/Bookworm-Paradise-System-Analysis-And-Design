5dd0675420f5b770493f365d65615d98246b2dce
AngJianming <angjianming2005@hotmail.com>
2024-11-27 22:17:53 +0800

    updates

--- products/apps.py
+++
@@ -1,8 +0,0 @@
-""" products app configuration file """
-from django.apps import AppConfig
-
-
-class ProductsConfig(AppConfig):
-    """ products app configuration """
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
