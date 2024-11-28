2578326d2d674a48d9c8c1e6ef71abf6137ac046
AngJianming <angjianming2005@hotmail.com>
2024-11-28 12:43:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -19,14 +19,14 @@ if(isset($_POST['submit'])){
          $_SESSION['admin_name'] = $row['name'];
          $_SESSION['admin_email'] = $row['email'];
          $_SESSION['admin_id'] = $row['id'];
-         header('location:admin_page.php');
+         header('location:/admin/admin_page.php');
 
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
          $_SESSION['user_email'] = $row['email'];
          $_SESSION['user_id'] = $row['id'];
-         header('location:home.php');
+         header('location:/user/home.php');
 
       }
 
